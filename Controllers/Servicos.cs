964a936f7428601e409a4cf9cfeb8b27a76ae5f7
theuz6 <msantossilva005@gmail.com>
2022-11-08 23:12:21 -0300

    Controllers update

---
+++ Controllers/Servicos.cs
@@ -0,0 +1,26 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using Sigma_Pets.Models;
+
+namespace Sigma_Pets.Controllers;
+
+public class ServicosController : Controller
+{
+    private readonly ILogger<ServicosController> _logger;
+
+    public ServicosController(ILogger<ServicosController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
