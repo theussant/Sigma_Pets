b07905e8588ac7dadd180853ff9aefdb9f1cc23c
theus6 <msantossilva005@gmail.com>
2022-11-17 11:27:39 -0300

    Remoção de interrogação

--- Models/ErrorViewModel.cs
+++ Models/ErrorViewModel.cs
@@ -2,7 +2,7 @@ namespace Sigma_Pets.Models;
 
 public class ErrorViewModel
 {
-    public string? RequestId { get; set; }
+    public string RequestId { get; set; }
 
     public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
 }
